18cc6822b3f9095370c74968c6b5c3d643c253a5
314302 <314302@0338S-S-R0B6H0K.sdcs.local>
2019-12-10 14:23:52 -0800

    Body part quiz almost finished

--- src/body_part_quiz/BodyPartQuiz.java
+++ src/body_part_quiz/BodyPartQuiz.java
@@ -59,22 +59,23 @@ public class BodyPartQuiz {
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-		if (guess = "Arnold") {
+		if (guess == "Arnold") {
 		score = +1;
 		}
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
-		//else {
-		//JOptionPane.showMessageDialog(null, "You're wrong. It's ");
+		else {
+		JOptionPane.showMessageDialog(null, "You're wrong. It's Arnold.");
 		}
+	
 		// 7. Use the showNextImage() method below to get the next image
-		//showNextImage();
+		showNextImage();
 
 		// 8. .... repeat 4-7 for all your images.....
-
+		
 		// 9. Show them their current score
 		
-	//}
+	}
 
 	public void showNextImage() {
 		panel.removeAll();
