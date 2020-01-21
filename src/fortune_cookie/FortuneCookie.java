a0a3b6c3707d2e0d0ea605c11c6cdc737e4238f4
314302 <314302@0338S-S-R0B6H0K.sdcs.local>
2020-01-21 14:22:49 -0800

    fortune cookie in progress

---
+++ src/fortune_cookie/FortuneCookie.java
@@ -0,0 +1,15 @@
+package fortune_cookie;
+
+public class FortuneCookie {
+
+	public void showButton() {
+	     System.out.println("Button clicked");
+	    }
+	public static void main(String[] args) {
+	      new FortuneCookie().showButton();
+	      
+	   }
+
+	
+	
+}
