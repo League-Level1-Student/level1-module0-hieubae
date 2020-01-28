8c7140d09d1e51b8f2a4f475923034474a98fe5b
314302 <314302@0338S-S-R0B6H0K.sdcs.local>
2020-01-28 14:24:24 -0800

    fortune cookie is progress

--- src/fortune_cookie/FortuneCookie.java
+++ src/fortune_cookie/FortuneCookie.java
@@ -1,15 +1,22 @@
 package fortune_cookie;
 
+import javax.swing.JFrame;
+
 public class FortuneCookie {
 
 	public void showButton() {
+		JButton button = new JButton();
+		
+		JFrame frame = new JFrame();
+		 frame.add(button);
+	     frame.pack(); 
+		     frame.setVisible(true);
 	     System.out.println("Button clicked");
+	     
 	    }
-	public static void main(String[] args) {
-	      new FortuneCookie().showButton();
-	      
+
 	   }
 
 	
 	
-}
+
---
+++ src/fortune_cookie/FortuneCookieRunner.java
@@ -0,0 +1,11 @@
+package fortune_cookie;
+
+public class FortuneCookieRunner {
+	public static void main(String[] args) {
+	      new FortuneCookie().showButton();
+
+	      
+	      
+	      
+	}
+}
